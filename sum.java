a70f769dfaa3b219ccee884dc807ab5c69641c7a
Megzz36 <157671136+Megzz36@users.noreply.github.com>
2024-01-25 11:47:07 +0530

    addition

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum{
+    public static void main(String[]args)
+    {
+        int num 1 = 15;
+        int num 2 = 20;
+        int sum = num 1 + num 2;
+        System.out.println(sum);
+
+    }
+}
